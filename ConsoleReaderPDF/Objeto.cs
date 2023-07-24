ff278fa086e6ac1d2b15053de364c1324462b5a8
Samuel Soares Guimarães <samuel.soares@unidesk.com.br>
2023-07-24 15:09:25 -0300

    parte do habitese

---
+++ ConsoleReaderPDF/Objeto.cs
@@ -0,0 +1,8 @@
+﻿namespace ConsoleReaderPDF
+{
+    public class Objeto
+    {
+        public string Nome { get; set; } = string.Empty;
+        public string Texto { get; set; } = string.Empty;
+    };
+}
--- ConsoleReaderPDF/Program.cs
+++ ConsoleReaderPDF/Program.cs
@@ -3,72 +3,114 @@
 using iTextSharp.text.pdf;
 using iTextSharp.text.pdf.parser;
 using IronXL;
-
+using ConsoleReaderPDF;
 
 var path = "C:\\Trabalho\\Arquivos\\prefeitura\\alvarahabilite";
-List<string> list = new ();
-//bool habiteses = false;
-//bool alvara = false;
-var contador = 1;
-
+List<Objeto> list = new ();
 string[] arquivos = Directory.GetFiles(path);
 
 #region Pegando arquivos e salvando em lista
+
 foreach (string arq in arquivos)
 {
     using PdfReader leitor = new(arq);
     for (int i = 1; i <= leitor.NumberOfPages; i++)
     {
         if(i == 1)
-            list.Add(PdfTextExtractor.GetTextFromPage(leitor, i));
+        {
+            Objeto objeto = new();
+            objeto.Nome = System.IO.Path.GetFileName(arq);
+            objeto.Texto = PdfTextExtractor.GetTextFromPage(leitor, i).ToString();
+            list.Add(objeto);
+        }
+            
     }
 
 }
 
-foreach (var item in list)
-{
-    if (item.Contains("HABITE-SE"))
-    {
-        System.Console.WriteLine("linha: " + contador + " HABITE-SE");
-    }
-    if (item.Contains("ALVARÁ"))
-    {
-        System.Console.WriteLine("linha: " + contador + " ALVARÁ");
-    }
-    contador++;
-}
 #endregion
 
 #region habitese
 
 WorkBook workbookHabitese = WorkBook.Create(ExcelFileFormat.XLSX);
-var sheetHabitese = workbookHabitese.CreateWorkSheet("Result Sheet");
-sheetHabitese["A1"].Value = "TITULO";
-sheetHabitese["B1"].Value = "ENDEREÇO DA OBRA";
-sheetHabitese["C1"].Value = "BAIRRO";
-sheetHabitese["D1"].Value = "CIDADE";
-sheetHabitese["E1"].Value = "PROPRIETÁRIO DO IMÓVEL (NOME)";
-sheetHabitese["F1"].Value = "PROPRIETÁRIO DO IMÓVEL (CPF)";
-sheetHabitese["G1"].Value = "RESPONSÁVEL PELA EXECUÇÃO DA OBRA (NOME)";
-sheetHabitese["H1"].Value = "RESPONSÁVEL PELA EXECUÇÃO DA OBRA (CPF)";
-sheetHabitese["I1"].Value = "RESPONSÁVEL TÉCNICO";
-sheetHabitese["J1"].Value = "DESCRICAO TERRENO";
-sheetHabitese["K1"].Value = "ESPECIFICAÇÃO (TIPO DE HABITE-SE)";
-sheetHabitese["L1"].Value = "ESPECIFICAÇÃO (DETALHES)";
-sheetHabitese["M1"].Value = "AREAS PRINCIPAIS";
-sheetHabitese["N1"].Value = "AREA TOTAL";
-sheetHabitese["O1"].Value = "OBSERVACAO";
-sheetHabitese["P1"].Value = "CIDADE/DATA";
+var sheetHabitese = workbookHabitese.CreateWorkSheet("HABITE-SE");
+
+sheetHabitese["A1"].Value = "NOME ARQUIVO";
+sheetHabitese["B1"].Value = "TITULO";
+sheetHabitese["C1"].Value = "ENDEREÇO DA OBRA";
+sheetHabitese["D1"].Value = "BAIRRO";
+sheetHabitese["E1"].Value = "CIDADE";
+sheetHabitese["F1"].Value = "PROPRIETÁRIO DO IMÓVEL (NOME)";
+sheetHabitese["G1"].Value = "PROPRIETÁRIO DO IMÓVEL (CPF/CNPJ)";
+sheetHabitese["H1"].Value = "RESPONSÁVEL PELA EXECUÇÃO DA OBRA (NOME)";
+sheetHabitese["I1"].Value = "RESPONSÁVEL PELA EXECUÇÃO DA OBRA (CPF/CNPJ)";
+sheetHabitese["J1"].Value = "RESPONSÁVEL TÉCNICO";
+sheetHabitese["K1"].Value = "DESCRICAO TERRENO";
+sheetHabitese["L1"].Value = "ESPECIFICAÇÃO (TIPO DE HABITE-SE)";
+//sheetHabitese["M1"].Value = "ESPECIFICAÇÃO (DETALHES)";
+//sheetHabitese["N1"].Value = "AREAS PRINCIPAIS";
+sheetHabitese["O1"].Value = "AREA TOTAL";
+sheetHabitese["P1"].Value = "OBSERVACAO";
 
 
-workbookHabitese.SaveAs($"{path}\\ExcelHabitese.xlsx");
+var linha = 2;
+var listaHabitese = list.Where(tipo => tipo.Texto.Contains("ENDEREÇO DA OBRA:")).ToList();
+foreach (var row in listaHabitese)
+{
+    sheetHabitese[$"A{linha}"].Value = $"{row.Nome}";    
+    sheetHabitese[$"B{linha}"].Value = $"HABITE-SE {row.Texto.Substring(row.Texto.IndexOf("Nº"), row.Texto.IndexOf("ENDEREÇO") - row.Texto.IndexOf("Nº"))}".Replace("\n", " ");
+    sheetHabitese[$"C{linha}"].Value = $"{row.Texto.Substring(row.Texto.IndexOf("ENDEREÇO DA OBRA:"), row.Texto.IndexOf("BAIRRO:") - row.Texto.IndexOf("ENDEREÇO DA OBRA:"))}".Replace("ENDEREÇO DA OBRA:", "").Replace("\n", " ");
+    sheetHabitese[$"D{linha}"].Value = $"{row.Texto.Substring(row.Texto.IndexOf("BAIRRO:"), row.Texto.IndexOf("CIDADE:") - row.Texto.IndexOf("BAIRRO:"))}".Replace("BAIRRO:", "").Replace("\n", " ");
+    sheetHabitese[$"E{linha}"].Value = $"{row.Texto.Substring(row.Texto.IndexOf("CIDADE:"), row.Texto.IndexOf("PROPRIETÁRIO DO IMÓVEL:") - row.Texto.IndexOf("CIDADE:"))}".Replace("CIDADE:", "").Replace("\n", " ");
+
+    var ProprietarioImovel = $"{row.Texto.Substring(row.Texto.IndexOf("PROPRIETÁRIO DO IMÓVEL:"), row.Texto.IndexOf("RESPONSÁVEL PELA EXECUÇÃO DA OBRA:") - row.Texto.IndexOf("PROPRIETÁRIO DO IMÓVEL:"))}";
+    if(ProprietarioImovel.IndexOf("CNPJ:") != -1)
+    {
+        sheetHabitese[$"F{linha}"].Value = $"{ProprietarioImovel.Substring(ProprietarioImovel.IndexOf("NOME:"), ProprietarioImovel.IndexOf("CNPJ:") - ProprietarioImovel.IndexOf("NOME:"))}".Replace("NOME:", "").Replace("\n", " ");
+        sheetHabitese[$"G{linha}"].Value = $"{ProprietarioImovel.Substring(ProprietarioImovel.IndexOf("CNPJ:"), ProprietarioImovel.Length - ProprietarioImovel.IndexOf("CNPJ:"))}".Replace("CNPJ:", "").Replace("\n", " ");
+    }
+    else if (ProprietarioImovel.IndexOf("CPF:") != -1)
+    {
+        sheetHabitese[$"F{linha}"].Value = $"{ProprietarioImovel.Substring(ProprietarioImovel.IndexOf("NOME:"), ProprietarioImovel.IndexOf("CPF:") - ProprietarioImovel.IndexOf("NOME:"))}".Replace("NOME:", "").Replace("\n", " ");
+        sheetHabitese[$"G{linha}"].Value = $"{ProprietarioImovel.Substring(ProprietarioImovel.IndexOf("CPF:"), ProprietarioImovel.Length - ProprietarioImovel.IndexOf("CPF:"))}".Replace("CPF:", "").Replace("\n", " ");
+
+    }
+
+    var ResponsavelExecucaoObra = $"{row.Texto.Substring(row.Texto.IndexOf("RESPONSÁVEL PELA EXECUÇÃO DA OBRA:"), row.Texto.IndexOf("RESPONSÁVEL TÉCNICO:") - row.Texto.IndexOf("RESPONSÁVEL PELA EXECUÇÃO DA OBRA:"))}";
+    if (ResponsavelExecucaoObra.IndexOf("CNPJ:") != -1)
+    {
+        sheetHabitese[$"H{linha}"].Value = $"{ResponsavelExecucaoObra.Substring(ResponsavelExecucaoObra.IndexOf("NOME:"), ResponsavelExecucaoObra.IndexOf("CNPJ:") - ResponsavelExecucaoObra.IndexOf("NOME:"))}".Replace("NOME:", "").Replace("\n", " ");
+        sheetHabitese[$"I{linha}"].Value = $"{ResponsavelExecucaoObra.Substring(ResponsavelExecucaoObra.IndexOf("CNPJ:"), ResponsavelExecucaoObra.Length - ResponsavelExecucaoObra.IndexOf("CNPJ:"))}".Replace("CNPJ:", "").Replace("\n", " ");
+
+    }else if (ResponsavelExecucaoObra.IndexOf("CPF:") != -1)
+    {
+        sheetHabitese[$"H{linha}"].Value = $"{ResponsavelExecucaoObra.Substring(ResponsavelExecucaoObra.IndexOf("NOME:"), ResponsavelExecucaoObra.IndexOf("CPF:") - ResponsavelExecucaoObra.IndexOf("NOME:"))}".Replace("NOME:", "").Replace("\n", " ");
+        sheetHabitese[$"I{linha}"].Value = $"{ResponsavelExecucaoObra.Substring(ResponsavelExecucaoObra.IndexOf("CPF:"), ResponsavelExecucaoObra.Length - ResponsavelExecucaoObra.IndexOf("CPF:"))}".Replace("CPF:", "").Replace("\n", " ");
+
+    }
+
+
+    sheetHabitese[$"J{linha}"].Value = $"{row.Texto.Substring(row.Texto.IndexOf("RESPONSÁVEL TÉCNICO:"), row.Texto.IndexOf("Conforme") - row.Texto.IndexOf("RESPONSÁVEL TÉCNICO:"))}".Replace("RESPONSÁVEL TÉCNICO:", "").Replace("\n", " ");
+    sheetHabitese[$"K{linha}"].Value = $"{row.Texto.Substring(row.Texto.IndexOf("Conforme"), row.Texto.IndexOf("ESPECIFICAÇÃO:") - row.Texto.IndexOf("Conforme"))}".Replace("\n", " ");
+    
+    var especificacoes = $"{row.Texto.Substring(row.Texto.IndexOf("ESPECIFICAÇÃO:"), row.Texto.IndexOf("Área total da obra:") - row.Texto.IndexOf("ESPECIFICAÇÃO:"))}".Replace("\n", " ");
+    sheetHabitese[$"L{linha}"].Value = $"{especificacoes.Substring(especificacoes.IndexOf("TIPO DE HABITE-SE:"), especificacoes.IndexOf("Dados da obra:") - especificacoes.IndexOf("TIPO DE HABITE-SE:"))}".Replace("TIPO DE HABITE-SE:", "").Replace("\n", " ");
+    //sheetHabitese[$"M{linha}"].Value = "";
+    //sheetHabitese[$"N{linha}"].Value = "";
+    sheetHabitese[$"O{linha}"].Value = $"{row.Texto.Substring(row.Texto.IndexOf("Área total da obra:"), row.Texto.IndexOf("OBSERVAÇÃO:") - row.Texto.IndexOf("Área total da obra:"))}".Replace("Área total da obra:", "").Replace("\n", " ");
+    sheetHabitese[$"P{linha}"].Value = $"{row.Texto.Substring(row.Texto.IndexOf("OBSERVAÇÃO:"), row.Texto.IndexOf("MG, em") - row.Texto.IndexOf("OBSERVAÇÃO:"))}".Replace("OBSERVAÇÃO:", "").Replace("\n", " ");    
+    linha++;
+}
+
+
+workbookHabitese.SaveAs($"{path}\\resultado\\ExcelHabitese.xlsx");
 
 #endregion
 
 #region alvara
-
+/*
 WorkBook workbookAlvara = WorkBook.Create(ExcelFileFormat.XLSX);
-var sheetAlvara = workbookAlvara.CreateWorkSheet("Result Sheet");
+var sheetAlvara = workbookAlvara.CreateWorkSheet("ALVARA");
 sheetAlvara["A1"].Value = "TITULO";
 sheetAlvara["B1"].Value = "PROPRIETÁRIO DO IMÓVEL (NOME)";
 sheetAlvara["C1"].Value = "PROPRIETÁRIO DO IMÓVEL (CPF)";
@@ -87,7 +129,7 @@ sheetAlvara["O1"].Value = "OBSERVACAO";
 sheetAlvara["P1"].Value = "LEI";
 
 workbookAlvara.SaveAs($"{path}\\ExcelAlvara.xlsx");
-
+*/
 #endregion
 
 
@@ -114,6 +156,20 @@ if (alvara)
 for (var index = 0; index < list.Count; index++)
 {
     System.Console.WriteLine("linha: " + index + " texto: " + list[index]);
+}
+
+
+foreach (var item in list)
+{
+    if (item.Contains("HABITE-SE"))
+    {
+        System.Console.WriteLine("linha: " + contador + " HABITE-SE");
+    }
+    if (item.Contains("ALVARÁ"))
+    {
+        System.Console.WriteLine("linha: " + contador + " ALVARÁ");
+    }
+    contador++;
 }
 */
 #endregion
