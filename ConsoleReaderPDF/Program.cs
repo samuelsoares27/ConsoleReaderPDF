78d46fe0f86e7ab6b520c0e9e7022d8a5f218219
Samuel Soares Guimarães <samuel.soares@unidesk.com.br>
2023-07-24 08:49:56 -0300

    Melhorias na leitura do pdf

--- ConsoleReaderPDF/Program.cs
+++ ConsoleReaderPDF/Program.cs
@@ -2,33 +2,54 @@
 
 using iTextSharp.text.pdf;
 using iTextSharp.text.pdf.parser;
+using System;
+using System.Reflection;
 using System.Text;
 
 
 List<string> list = new ();
-
 bool habiteses = false;
 bool alvara = false;
+var contador = 1;
 
-using (PdfReader leitor = new ("C:\\temp\\Habitese - 2021-04-20T151854.374.pdf"))
+string[] arquivos = Directory.GetFiles("C:\\Trabalho\\Arquivos\\prefeitura\\alvarahabilite");
+
+Console.WriteLine("Arquivos:");
+foreach (string arq in arquivos)
 {
-    StringBuilder texto = new ();
+    using PdfReader leitor = new(arq);
     for (int i = 1; i <= leitor.NumberOfPages; i++)
     {
-        list.Add(PdfTextExtractor.GetTextFromPage(leitor, i));
-
-        if (list[i-1].Contains("HABITE-SE"))
-        {
-            habiteses = true;
-        }
-        if (list[i-1].Contains("ALVARÁ"))
-        {
-            alvara = true;
-        }
+        if(i == 1)
+            list.Add(PdfTextExtractor.GetTextFromPage(leitor, i));
     }
 
 }
 
+foreach (var item in list)
+{
+    if (item.Contains("HABITE-SE"))
+    {
+        System.Console.WriteLine("linha: " + contador + " HABITE-SE");
+    }
+    if (item.Contains("ALVARÁ"))
+    {
+        System.Console.WriteLine("linha: " + contador + " ALVARÁ");
+    }
+    contador++;
+}
+
+
+#region salvando código
+/*
+if (list[i - 1].Contains("HABITE-SE"))
+{
+    habiteses = true;
+}
+if (list[i - 1].Contains("ALVARÁ"))
+{
+    alvara = true;
+}
 if (habiteses)
 {
     System.Console.WriteLine("HABITE-SE");
@@ -43,4 +64,5 @@ for (var index = 0; index < list.Count; index++)
 {
     System.Console.WriteLine("linha: " + index + " texto: " + list[index]);
 }
-
+*/
+#endregion
